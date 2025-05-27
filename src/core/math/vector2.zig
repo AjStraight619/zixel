b1aab2b856d5456f7b47d48620aeff63e910f2be
Alex Straight <AlexStraight619@gmail.com>
2025-05-26 20:36:01 -0700

    refactor: make second param Vector2 type instead of self, remove init function, add scale function

--- src/core/math/vector2.zig
+++ src/core/math/vector2.zig
@@ -13,28 +13,35 @@ pub const Vector2 = struct {
 
     const Self = @This();
 
-    pub fn init(x: f32, y: f32) Self {
-        return Self{ .x = x, .y = y };
+    pub fn zero() Self {
+        return Self{ .x = 0, .y = 0 };
     }
 
-    pub fn add(self: Self, other: Self) Self {
+    pub fn add(self: Self, other: Vector2) Self {
         return Self{ .x = self.x + other.x, .y = self.y + other.y };
     }
 
-    pub fn sub(self: Self, other: Self) Self {
+    pub fn sub(self: Self, other: Vector2) Self {
         return Self{ .x = self.x - other.x, .y = self.y - other.y };
     }
 
-    pub fn mul(self: Self, other: Self) Self {
+    pub fn mul(self: Self, other: Vector2) Self {
         return Self{ .x = self.x * other.x, .y = self.y * other.y };
     }
 
-    pub fn div(self: Self, other: Self) Self {
+    pub fn div(self: Self, other: Vector2) Self {
         // Ensure no division by zero
         if (other.x == 0 or other.y == 0) {
             @panic("Division by zero");
         }
         return Self{ .x = self.x / other.x, .y = self.y / other.y };
+    }
+
+    pub fn scale(self: Self, scalar: f32) Vector2 {
+        return Vector2{
+            .x = self.x * scalar,
+            .y = self.y * scalar,
+        };
     }
 
     pub fn len(self: Self) f32 {
@@ -49,11 +56,11 @@ pub const Vector2 = struct {
         return Self{ .x = self.x / length, .y = self.y / length };
     }
 
-    pub fn dot(self: Self, other: Self) f32 {
+    pub fn dot(self: Self, other: Vector2) f32 {
         return self.x * other.x + self.y * other.y;
     }
 
-    pub fn cross(self: Self, other: Self) f32 {
+    pub fn cross(self: Self, other: Vector2) f32 {
         return self.x * other.y - self.y * other.x;
     }
 
@@ -61,19 +68,19 @@ pub const Vector2 = struct {
         return Self{ .x = self.x * @cos(angle) - self.y * @sin(angle), .y = self.x * @sin(angle) + self.y * @cos(angle) };
     }
 
-    pub fn lerp(self: Self, other: Self, t: f32) Self {
+    pub fn lerp(self: Self, other: Vector2, t: f32) Self {
         return Self{ .x = math.lerp(self.x, other.x, t), .y = math.lerp(self.y, other.y, t) };
     }
 
-    pub fn distance(self: Self, other: Self) f32 {
+    pub fn distance(self: Self, other: Vector2) f32 {
         return math.sqrt((self.x - other.x) * (self.x - other.x) + (self.y - other.y) * (self.y - other.y));
     }
 
-    pub fn reflect(self: Self, normal: Self) Self {
+    pub fn reflect(self: Self, normal: Vector2) Self {
         return self.sub(normal.mul(self.dot(normal)));
     }
 
-    pub fn project(self: Self, other: Self) Self {
+    pub fn project(self: Self, other: Vector2) Self {
         if (other.len() == 0) {
             @panic("Cannot project onto zero vector");
         }
