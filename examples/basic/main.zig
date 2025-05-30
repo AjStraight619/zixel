82304c4e280b1d9e519ea4a179b4d9bc6d2f26f2
Alex Straight <AlexStraight619@gmail.com>
2025-05-29 19:29:03 -0700

    Add basic example

--- examples/basic/main.zig
+++ examples/basic/main.zig
@@ -6,18 +6,28 @@ const Body = zig2d.Body;
 const Vector2 = zig2d.Vector2;
 const DynamicBody = zig2d.DynamicBody;
 const rl = @import("raylib");
-pub fn main() void {
+
+const EngineConfig = zig2d.engine.EngineConfig;
+const GameAction = zig2d.GameAction;
+
+pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
     // Default engine config
     var game = try Engine.init(allocator, .{});
 
-    _ = Body.initStatic(.{
-        .rect = .{ .width = 100, .height = 100, .color = rl.Color.red },
-    }, Vector2.zero(), .{});
+    const x = 100;
+    const y = 100;
+
+    const width = 100;
+    const height = 100;
+
+    const rect1 = Rectangle.init(x, y, width, height);
+
+    _ = try game.physics.addBody(Body.initStatic(.{ .rectangle = rect1 }, Vector2.zero(), .{}));
 
     defer game.deinit();
 
-    game.run();
+    try game.run();
 }
