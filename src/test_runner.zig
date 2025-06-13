38999f5d70318f04e90106d2869d898ccf800072
Alex Straight <AlexStraight619@gmail.com>
2025-06-12 22:48:44 -0700

    add new files to test_runner

--- src/test_runner.zig
+++ src/test_runner.zig
@@ -8,9 +8,9 @@ comptime {
     _ = @import("physics/world.zig");
     _ = @import("physics/body.zig");
     _ = @import("input/keybinds.zig");
-    _ = @import("core/math/shapes.zig");
-    _ = @import("core/math/aabb.zig");
-    _ = @import("renderer/window.zig");
-    _ = @import("renderer/config.zig");
+    _ = @import("math/shapes.zig");
+    _ = @import("math/aabb.zig");
+    _ = @import("graphics/window.zig");
+    _ = @import("graphics/config.zig");
     _ = @import("physics/config.zig");
 }
