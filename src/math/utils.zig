104024d05a82f112f3034b2e735b705159e75673
Alex Straight <AlexStraight619@gmail.com>
2025-06-16 14:03:55 -0700

    remove unused utils

--- src/math/utils.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-
-pub fn degreesToRadians(degrees: f32) f32 {
-    return degrees * -(std.math.pi / 180.0);
-}
-
-pub fn radiansToDegrees(radians: f32) f32 {
-    return radians * (180.0 / std.math.pi);
-}
